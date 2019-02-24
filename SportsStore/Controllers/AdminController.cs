3def5c6bd395e70ffd757182959e9e077d918034
joe barker <joebarker@mail.weber.edu>
2019-02-24 16:19:46 -0700

    up to page 328

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
-using System.Linq;
+
 namespace SportsStore.Controllers
 {
     public class AdminController : Controller
